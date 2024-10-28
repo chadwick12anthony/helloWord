372adb2522224763d9397fb608229e7528751844
jo markov <chadwick12anthony@gmail.com>
2024-10-28 13:57:54 +0300

    Initial Commit

---
+++ helloWord/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloWord
+//
+//  Created by Anthony Chadwick on 28.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
